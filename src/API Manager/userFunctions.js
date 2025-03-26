5ba797c43ef4d7c134a3b095ac0149b50adcc6d2
prakhart07 <prakhart073@gmail.com>
2025-03-26 12:47:33 +0530

    data format modified

--- src/API Manager/userFunctions.js
+++ src/API Manager/userFunctions.js
@@ -4,7 +4,7 @@ import { url } from "./url";
 export async function loginUser(data) {
     try {
       console.log("data inside loginuser:",data);
-      const res= await axios.post(`${url}/login`, {data});
+      const res= await axios.post(`${url}/login`, data);
       console.log("res user login:",res);
       return res;
     } catch (error) {
